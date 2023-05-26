b956d4cee4f44a36e95fc8294b150bb9c2ac38ea
rgligora <99548015+rgligora@users.noreply.github.com>
2023-05-26 12:35:05 +0200

    updated app.js

--- app.js
+++ app.js
@@ -3,8 +3,32 @@ const {readFile} = require('fs');
 const http = require('http')
 var counter = 1
 
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf-8', (err, data)=> {
+            if (err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        })
+    })
+}
+const aboutFunction = async () => {
+    try {
+        const first = await getText('./content/aboutPage.txt')
+        console.log("Loaded first file")
+        const second = await getText('./content/aboutPageAdditionalInfo.txt')
+        console.log("Loaded second file")
+        console.log("About page loaded")
+        return first + second
+    } catch (error) {
+        console.log(error)
+    }
+}
+
 const server = http.createServer((req,res) => {
-    if(req.url !== '/favicon.ico'){
+    if(req.url === '/'){
         console.log(`Starting task: ${counter}`)
         var first_task = counter
         readFile('./content/first.txt', 'utf-8', (err, data) => {
@@ -22,6 +46,11 @@ const server = http.createServer((req,res) => {
         }, 0)
         counter++
         res.end("Closing down")
+    }
+    if(req.url === '/about'){
+        aboutFunction().then((data)=>{
+            res.end(data)
+        })
     }
 })
 
